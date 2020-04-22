7ea10050c3338587b4a30784e519cd9580116b0f
Gernby <gernby2002@yahoo.com>
2020-04-21 19:55:27 -0500

    fixed the other angle input

--- selfdrive/controls/transcoderd.py
+++ selfdrive/controls/transcoderd.py
@@ -158,10 +158,10 @@ while 1:
   left_center = descaled_output[:,1:2]
   right_center = descaled_output[:,2:3]
   #calc_center = (l_prob * descaled_output[:,1:2] + r_prob * descaled_output[:,2:3]) / (l_prob + r_prob + 0.0005)
-  calc_center = descaled_output[:,0:1]
+  calc_center = descaled_output[:,4:5] * lr_prob
 
   if abs(cs.steeringTorque) < 1200 and abs(cs.adjustedAngle) < 30:
-    upper_limit = one_deg_per_sec * cs.vEgo * (max(2, min(5, abs(cs.steeringRate))) + accel_counter)
+    upper_limit = one_deg_per_sec * cs.vEgo * lr_prob * (max(2, min(5, abs(cs.steeringRate))) + accel_counter)
     lower_limit = -upper_limit
     if cs.torqueRequest >= 1:
       upper_limit = one_deg_per_sec * cs.steeringRate
@@ -174,7 +174,7 @@ while 1:
       lower_limit = lower_limit + angle
     if l_prob + r_prob > 0:
       accel_counter = max(0, min(2, accel_counter - 1))
-      angle = np.clip((descaled_output[:,3:4] - descaled_output[0,3:4]) * (1 + advanceSteer), lower_limit, upper_limit)
+      angle = np.clip((descaled_output[:,0:1] - descaled_output[0,0:1]) * (1 + advanceSteer), lower_limit, upper_limit)
       #angle = np.clip((descaled_output[:,3:4] - descaled_output[0,3:4]) * (1 + advanceSteer), lower_limit, upper_limit)
     else:
       accel_counter = max(0, min(2, accel_counter + 1))
