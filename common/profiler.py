8501a37e3b199e93f26ed385c9f3d055db9cf63f
Roberto Cruz <roberto.cruz@ontruck.com>
2020-08-30 13:07:06 +0200

    Update common directory from master OP

--- common/profiler.py
+++ common/profiler.py
@@ -1,15 +1,14 @@
 import time
 
-class Profiler(object):
-  def __init__(self, enabled=False, source=''):
+class Profiler():
+  def __init__(self, enabled=False):
     self.enabled = enabled
     self.cp = {}
-    self.source = source
     self.cp_ignored = []
     self.iter = 0
     self.start_time = time.time()
     self.last_time = self.start_time
-    self.tot = 0.00001
+    self.tot = 0.
 
   def reset(self, enabled=False):
     self.enabled = enabled
@@ -18,7 +17,6 @@ class Profiler(object):
     self.iter = 0
     self.start_time = time.time()
     self.last_time = self.start_time
-    self.tot = 0.00001
 
   def checkpoint(self, name, ignore=False):
     # ignore flag needed when benchmarking threads with ratekeeper
@@ -38,11 +36,10 @@ class Profiler(object):
     if not self.enabled:
       return
     self.iter += 1
-    #print("******* Profiling *******")
+    print("******* Profiling *******")
     for n, ms in sorted(self.cp.items(), key=lambda x: -x[1]):
-      if not n in self.cp_ignored:
-      #  print("%30s: %7.2f   percent: %3.0f   IGNORED" % (n, ms*1000.0, ms/self.tot*100))
-      #else:
-        print("%s - %30s: %9.1f   percent: %3.0f" % (self.source, n, ms*1000.0, ms/self.tot*100))
-    #print("Iter clock: %2.6f   TOTAL: %2.2f" % (self.tot/self.iter, self.tot))
-
+      if n in self.cp_ignored:
+        print("%30s: %9.2f   percent: %3.0f   IGNORED" % (n, ms*1000.0, ms/self.tot*100))
+      else:
+        print("%30s: %9.2f   percent: %3.0f" % (n, ms*1000.0, ms/self.tot*100))
+    print("Iter clock: %2.6f   TOTAL: %2.2f" % (self.tot/self.iter, self.tot))
